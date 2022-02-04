4da02a85991f162a7d8fa48cabf6105370323532
HOUNKPATI <92785569+HOUNKPATI@users.noreply.github.com>
2022-02-04 09:59:40 +0100

    les variables

---
+++ eclipse-workspace/affiche/src/affiche/exoaffiche.java
@@ -0,0 +1,26 @@
+package affiche;
+
+public class exoaffiche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Voici un exemple de programme simple en Java.");
+		System.out.println("STOP.");
+		
+		System.out.println("Ce programme a pour objectif de comprendre comment fonctionne le (pritln) dans JAVA.");
+		System.out.println("STOP.");
+		
+		System.out.println("JE COMMENCE PAR CODER EN JAVA.");
+		System.out.println("STOP.");
+		
+		System.out.println("Ne pas oublier le (ln) pour demander (print) le passage à la ligne.");
+		System.out.println("STOP.");
+		
+		System.out.println("Analyse du programme\r\n"
+				+ "Les six premières lignes du programme, commençant par les caractères //, sont des commentaires. Ils n'affectent en rien la tâche du programme, mais sont utiles pour que le code source du programme soit plus compréhensible par les humains.");
+
+	}
+	
+
+}
