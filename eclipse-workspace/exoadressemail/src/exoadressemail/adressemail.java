22d0cab3b3c510c9e0621635c8e1f74f5725823d
HOUNKPATI <92785569+HOUNKPATI@users.noreply.github.com>
2022-02-25 11:49:22 +0100

    exoadressemail

--- eclipse-workspace/exoadressemail/src/exoadressemail/adressemail.java
+++ eclipse-workspace/exoadressemail/src/exoadressemail/adressemail.java
@@ -1,49 +1,59 @@
 package exoadressemail;
 
+import java.util.Scanner;
+
 public class adressemail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
 
 	
 	//DECLARATION
 	
 		//Reel prenom
-		float prenom;
+		String prenom;
 	
 		//Reel nom
-		float nom;
+		String nom;
 	
 		//Reel un numero de departement
-		float unNumeroDeDepartement;
+		String unNumeroDeDepartement;
 	
 		//Reel adresse mail
-		float adresseMail;
-	
+		String adresseMail;
+		
+		Scanner sc = new Scanner(System.in);
 	//Saisir
 		
-		//Ecrire ""
-		System.out.println("prenom");
+		//Ecrire "Donnez-moi s'il vous plaît un prenom"
+		System.out.println("Donnez-moi s'il vous plaît un prenom");
 		
 		//Lire "prenom"
 		prenom = sc.next();
 		
 		//Ecrire "nom"
-		System.out.println("nom");
+		System.out.println("Donnez-moi s'il vous plaît un nom");
 		
 		//Lire "nom"
 		
-		nom = scanner.next();
+		nom = sc.next();
 		
 		
-		///ecrire "Donnez-moi SVP un prenom"
-		System.out.println("Donnez-moi SVP un prenom");
+		//ecrire "Donnez-moi SVP un N° de Département"
+		System.out.println("Donnez-moi SVP un N° de Département");
 		
-		//Lire un prenom;
+		//Lire "unNumeroDeDepartement"
 		
+		unNumeroDeDepartement = sc.next();
 		
+		//ecrire "Donnez-moi SVP une adresseMail"
+		System.out.println("Donnez-moi SVP une adresse mail");
+				
+		//Lire "adresseMail"
 		
+		adresseMail = sc.next();
+				
+	}
 	
 }
